69b6fb3c0d6b643aaa8c70bc41080d9671d198a9
Aline Machado <alinedasmachado@gmail.com>
2023-02-28 13:04:11 -0300

    Exercicios novos

--- ex02.js
+++ ex02.js
@@ -1,10 +1,3 @@
-let numero1 = 5;
-let numero2 = 7
-let numero3 = 88
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
 
-if(numero1 %2==0 || numero2 %2==0|| numero3 %2==0){
-    console.log(true);
-} else{
-    console.log(false);
-}
-
+for()
--- javascript.js
+++ javascript.js
@@ -64,3 +64,22 @@ if(numero1 %2==0 || numero2 %2==0|| numero3 %2==0){
 } else{
     console.log(false);
 }
+
+let numero1 = 5;
+let numero2 = 7
+let numero3 = 88
+
+if(numero1 %2==0 || numero2 %2==0|| numero3 %2==0){
+    console.log(true);
+} else{
+    console.log(false);
+}
+
+let menu = ['pizza', 'burger', 'cachorro quente', 'yakissoba', 'guioza',]
+
+
+menu.push('carne');
+
+menu[6] = 'chocolate';
+
+console.log(menu);
